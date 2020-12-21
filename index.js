361259b7d33402994eb46629f1f6b782af0ca44a
RyuK-H <ryugihyeok@gmail.com>
2020-12-21 14:35:05 +0900

    ENH: nodemon 추가, key.js로 mongoDB URI 관리

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 const setting = require('./setting.json')
+const { User } = require("./models/User")
+const bodyParser = require('body-parser')
+const config = require("./config/key")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://ryuk:${setting.password}@ryuk-backend.fzdo5.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,6 +22,17 @@ mongoose.connect(`mongodb+srv://ryuk:${setting.password}@ryuk-backend.fzdo5.mong
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post("/register", (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    });
 })
 
 app.listen(port, () => {
