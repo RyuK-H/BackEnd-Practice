86fb1582d2a1403aca5799a119404d3c5560abb5
RyuK-H <ryugihyeok@gmail.com>
2020-12-21 13:39:25 +0900

    ENH: 몽고DB 연결

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const setting = require('./setting.json')
+
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://ryuk:${setting.password}@ryuk-backend.fzdo5.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("MongoDB Connected.."))
+  .catch(error => console.log(error))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
