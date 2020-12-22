82789d68a161c2bf3fc517a32c90dc10716efaa8
RyuK-H <ryugihyeok@gmail.com>
2020-12-22 16:34:04 +0900

    ENH: react route 기본 세팅

---
+++ client/src/components/views/LandingPage/index.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from "react";
+import axios from "axios";
+
+function LandingPage() {
+    useEffect(() => {
+        axios.get("http://localhost:5000//api/hello")
+        .then(response => console.log(response.data));
+    }, [])
+
+    return (
+        <div>
+            LadingPage
+        </div>
+    )
+}
+
+export default LandingPage;
